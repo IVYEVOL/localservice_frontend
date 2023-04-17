1a556d3a3db062480ab77ef62dd89263c028df86
ytzhang1024 <tim.y.zhang@outlook.com>
2023-04-17 15:01:16 +0100

    tmp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import MyLayout from "./components/MyLayout"
 import Dashboard from './pages/dashboard'
-import Users from './pages/user'
+import Users from './pages/customer/profile'
 
 function App() {
 
